32b568633009a1945f25fee360f33aecbea8e76f
Shajid1541 <shajid.kamal@brainstation-23.com>
2024-04-25 10:55:08 +0600

    faulty fact.cpp added

---
+++ fact.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial
+int factorial(int n) {
+    if (n <= 5)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else
+        cout << "Factorial of " << n << " = " << factorial(n);
+
+    return 0;
+}
